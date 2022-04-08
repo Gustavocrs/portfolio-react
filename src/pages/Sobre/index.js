8a4dbf3e84f865b090a46c17ae566570dce283d8
Gustavo CR. da Silva <gustavo@lekas.com.br>
2022-04-08 08:45:33 -0300

    atualização Sobre

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -4,32 +4,32 @@ import FotoPerfil from '../../img/gustavo.jpeg'
 export default function Sobre() {
   return (
     <section className="divSobre" id="sobremim">
-      <img
-        className="fotoPerfil"
-        src={FotoPerfil}
-        alt="Foto de perfil"
-      />
+      <img className="fotoPerfil" src={FotoPerfil} alt="Foto de perfil" />
 
       <div>
         <h1 className="tituloSobre">Um pouco sobre mim...</h1>
 
         <p className="textoSobre">
-          Olá! Me chamo Gustavo Silva, sou Gerente Administrativo em uma empresa
-          do ramo de Metrologia, atuo na mesma empresa desde 2008, passando por
-          todos os cargos administrativos até o atual como gestor. Sou formado
-          em Análise e Desenvolvimento de Sistemas, e busco diariamente
-          aperfeiçoar meus conhecimentos, seja com pesquisas ou com cursos.
+          Olá! Me chamo Gustavo Silva, sou formado em Análise e Desenvolvimento
+          de Sistemas, com ênfase em Desenvolvimento Front-end. 
+          Trabalho com:</p>
+          <p className='textoSobre'><mark>Front-end:</mark> HTML5, CSS3, Flexbox, Grid, SASS, Bootstrap, JavaScript,
+          React.</p>
+          <p className='textoSobre'><mark>Outros conhecimentos:</mark> Figma, SQL, Postgres e Oracle; Git e
+          GitHub. </p>
+          <p className='textoSobre'>Busco diariamente aperfeiçoar meus conhecimentos, seja com
+          pesquisas ou com cursos de especialização, como pode ser visualizado
+          na seção de Certificados. Atualmente, sou gestor em uma empresa do
+          ramo de Metrologia, onde atuo desde 2008, passando por todos os cargos
+          administrativos até o atual, respondendo diretamente a direção.
         </p>
 
         <p className="textoSobre">
-          Atualmente, direciono meu tempo para a área de Front-end, com HTML5 /
-          CSS3 / JavaScript / React. Possuo conhecimento em programação
-          orientada a objetos, com foco de aprendizado em Java SE. Considero me
-          bastante dinâmico e tenho bastante facilidade em aprender, possuo uma
-          boa capacidade de abstração, o que me ajuda a traduzir a necessidade
+          Considero me bastante dinâmico e tenho bastante facilidade em aprender, 
+          possuo uma boa capacidade de abstração, o que me ajuda a traduzir a necessidade
           do cliente em código.
         </p>
       </div>
     </section>
-  );
+  )
 }
