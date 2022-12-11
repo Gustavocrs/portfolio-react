7324ed5614c29e86a9876b69a27d517ed830a4df
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-12-10 22:02:30 -0300

    Alteração de logos bootstrap, mui e react

--- src/pages/Tecnologias/index.jsx
+++ src/pages/Tecnologias/index.jsx
@@ -3,11 +3,11 @@ import "./Tecnologias.css";
 import Logohtml from "../../img/tecs/html.png";
 import Logocss from "../../img/tecs/css.png";
 import Logosass from "../../img/tecs/sass.png";
-import Logobootstrap from "../../img/tecs/bootstrap.png";
+import Logobootstrap from "../../img/tecs/bootstrap5.svg";
 import Logojs from "../../img/tecs/js.png";
-import Logoreact from "../../img/tecs/react3.svg";
+import Logoreact from "../../img/tecs/react-colored.svg";
 import Logostyled from "../../img/tecs/styled.png";
-
+import Logomui from "../../img/tecs/mui.svg";
 import CardTecnologias from "./CardTecnologias";
 
 export default function Tecnologias() {
@@ -15,13 +15,26 @@ export default function Tecnologias() {
     <section className="divTecnologias" id="tecnologias">
       <p className="tituloTecnologias">Tecnologias Conhecidas</p>
       <div className="tecnologiasItem">
-        <CardTecnologias src={Logohtml} alt="Logo HTML5" nomeTec="HTML 5" />
-        <CardTecnologias src={Logocss} alt="Logo CSS3" nomeTec="CSS 3" />
-        <CardTecnologias src={Logojs} alt="Logo Javascript" nomeTec="Javascript" />
-        <CardTecnologias src={Logosass} alt="Logo SASS" nomeTec="SASS" />
-        <CardTecnologias src={Logobootstrap} alt="Logo Bootstrap" nomeTec="Bootstrap" />
         <CardTecnologias src={Logoreact} alt="Logo React" nomeTec="React" />
-        <CardTecnologias src={Logostyled} alt="Logo React" nomeTec="Styled Components" />
+        <CardTecnologias
+          src={Logostyled}
+          alt="Logo React"
+          nomeTec="Styled Components"
+        />
+        <CardTecnologias
+          src={Logojs}
+          alt="Logo Javascript"
+          nomeTec="Javascript"
+        />
+        <CardTecnologias src={Logomui} alt="Logo MUI" nomeTec="MUI" />
+        <CardTecnologias
+          src={Logobootstrap}
+          alt="Logo Bootstrap"
+          nomeTec="Bootstrap"
+        />
+        <CardTecnologias src={Logosass} alt="Logo SASS" nomeTec="SASS" />
+        <CardTecnologias src={Logocss} alt="Logo CSS3" nomeTec="CSS 3" />
+        <CardTecnologias src={Logohtml} alt="Logo HTML5" nomeTec="HTML 5" />
       </div>
     </section>
   );
